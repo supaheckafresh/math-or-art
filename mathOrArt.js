ad715bb835c0c0571da1d7281f673d8fa66c8d99
supaheckafresh <neilstrain@gmail.com>
2015-10-06 00:07:21 -0700

    Add error handling.

--- mathOrArt.js
+++ mathOrArt.js
@@ -2,12 +2,11 @@
 function mathOrArt() {
     var entry = prompt('Do you prefer math or art?').trim();
     if (entry == 'math' || entry == 'art') {
-        var preference = entry;
-        mathOrArt.promptBasedOnPreference(preference);
+        mathOrArt.promptBasedOnPreference(entry);
     } else {
         alert('I\'m sorry, I didn\'t recognize your answer! Please try again.');
     }
-};
+}
 
 mathOrArt.promptBasedOnPreference = function(preference) {
     if (preference == 'math')
@@ -18,15 +17,22 @@ mathOrArt.promptBasedOnPreference = function(preference) {
 
 function math() {
     var getNumbers = prompt('Please input two numbers you\'d like to add (separate with a space).');
-    var numbers = getNumbers.trim().split(' ');
-    var sum = parseFloat(numbers[0]) + parseFloat(numbers[1]);
-    alert('The sum of your two numbers is: ' + sum);
+    var numbers = getNumbers.trim().replace(/\s+/g, ' ').split(' ');
+    if (numbers.length !== 2) {
+        alert('Sorry -- I didn\'t two numbers there. Please try again!');
+        math();
+    } else {
+        var sum = parseFloat(numbers[0]) + parseFloat(numbers[1]);
+        console.log(numbers[0]);
+        console.log(numbers[1]);
+        alert('The sum of your two numbers is: ' + sum + ".");
+    }
 }
 
 function art() {
     var styles = art.getStyles();
     var favorites = art.getFavoriteOrder(styles);
-    alert('Your favorite art styles, in order, are ' + favorites[0] + ', followed by ' + favorites[1]);
+    alert('Your favorite art styles, in order, are ' + favorites[0] + ', followed by ' + favorites[1] + ".");
 }
 
 art.getStyles = function() {
@@ -40,14 +46,15 @@ art.getStyles = function() {
 };
 
 art.getFavoriteOrder = function(styles) {
+    var favorites = [];
     var entry = prompt('Which is your favorite art style -- ' + styles[0] + ', or ' + styles[1] + '?');
     if (entry == styles[0] || entry == styles[1]) {
         var favorite = entry;
         var secondFavorite = styles[Math.abs(styles.indexOf(favorite) - 1)];
+        favorites = [favorite, secondFavorite];
     } else {
         alert("Sorry, your favorite doesn't match your previous entries!");
-        //TODO - below returns undefined and undefined!
-        art.getFavoriteOrder(styles);
+        return art.getFavoriteOrder(styles);
     }
-    return [favorite, secondFavorite];
+    return favorites;
 };
